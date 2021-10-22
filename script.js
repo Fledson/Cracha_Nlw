9e7ca5a531ca2fd69e0d3518d0191a647ce0b41f
Fledson Henrique <fledsonhenrique@gmail.com>
2021-10-21 21:18:11 -0300

    Finalizando o Projeto

---
+++ script.js
@@ -0,0 +1,36 @@
+const linksSocialMedia = {
+    github: 'fledson',
+    youtube: 'fledson96',
+    facebook: 'fledsonhenrique',
+    instagram: 'fledson_henrique',
+    twitter: 'fledsonhenrique'
+}
+
+function changeSocialMediaLinks() {
+
+    for (let li of socialLinks.children) {
+        const social = li.getAttribute('class');
+        li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+    }
+
+
+}
+
+changeSocialMediaLinks();
+
+function getGitHubProfileInfos() {
+    const url = `https://api.github.com/users/${linksSocialMedia.github}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            userImage.src = data.avatar_url;
+            userName.textContent = data.name;
+            userLogin.textContent = data.login
+            userLink.href = data.html_url;
+            userBio.textContent = data.bio;
+        })
+
+}
+
+getGitHubProfileInfos()
