3645bb258bb03db25dcdc942e9ad6491135022f3
Fledson Henrique <fledsonhenrique@gmail.com>
2021-10-23 22:23:41 -0300

    finalizando o projeto

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function getGitHubProfileInfos() {
         .then(data => {
             userImage.src = data.avatar_url;
             userName.textContent = data.name;
-            userLogin.textContent = data.login
+            userLogin.textContent = data.login;
             userLink.href = data.html_url;
             userBio.textContent = data.bio;
         })
@@ -34,3 +34,15 @@ function getGitHubProfileInfos() {
 }
 
 getGitHubProfileInfos()
+
+function showQr() {
+    userImage.src = `https://chart.googleapis.com/chart?chs=100x100&cht=qr&chl=https://dowhile.io/convite/fledson`
+}
+
+function hideQr() {
+    getGitHubProfileInfos()
+}
+
+function linkDoWhile() {
+    window.open(`https://dowhile.io/convite/${linksSocialMedia.github}`)
+}
